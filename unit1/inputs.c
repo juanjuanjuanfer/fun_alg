08009237d5eac77bbe0d5a2494efc73807778ffb
juan <2109061@upy.edu.mx>
2022-01-28 11:33:20 -0600

    added inputs and variables

--- unit1/inputs.c
+++ unit1/inputs.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
-int num1; //variables declaration
+int num1; //variables declaration 
 int num2;
 
 int main(){
 
+    // Print messages //
     printf("entern number 1: \n\tnum1:  ");
-    scanf("%d", &num1);
-    printf("entern number 2: \n\tnum2:  ");
+    scanf("%d", &num1); //inputs jeyboard
+    printf("entern number 2: \n\tnum2:  ");// output screen//
     scanf("%d", &num2);
+
+    // process//
+    int sum = num1 + num2;
+    printf("suma = %d\n", sum);
+    
     return 0;
 }
---
+++ unit1/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sum.h>
+
+//DAtaType integer, float, string, characters//
+int edad = 24;
+float pi = 3.1416;
+char* name = "Juan Antonio"
+char sexo = 'M'
+
+int (){
+
+    return 0;
+}
