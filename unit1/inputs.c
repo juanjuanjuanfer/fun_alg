d12a205ebe0d0e30d9769794bc0f3938265b5ba4
juan <2109061@upy.edu.mx>
2022-01-28 10:18:45 -0600

    first c program

---
+++ unit1/inputs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int num1; //variables declaration
+int num2;
+
+int main(){
+
+    printf("entern number 1: \n\tnum1:  ");
+    scanf("%d", &num1);
+    printf("entern number 2: \n\tnum2:  ");
+    scanf("%d", &num2);
+    return 0;
+}
