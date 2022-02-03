4c35192d6ee53b3e6908e028f2904da624ca4000
juan <2109061@upy.edu.mx>
2022-02-03 10:40:59 -0600

    fixed some errors

--- unit1/variables.c
+++ unit1/variables.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <sum.h>
+#include "sum.h"
 
 //DAtaType integer, float, string, characters//
-int edad = 24;
+int edad = 19;
 float pi = 3.1416;
-char* name = "Juan Antonio"
-char sexo = 'M'
+char* name = "Juan Antonio";
+char sexo = 'M';
+int temp;
 
-int (){
-
+int main(){
+    int after10years = suma(edad,10);
+    printf("mi edad despues de 10 anos: %d", after10years);
     return 0;
 }
